10346a7c25ae5643cceb6c09989cbe27e0e0e8ba
`xeno <xlimo2002@gmail.com>
2023-09-12 15:38:14 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,44 @@
-local DarkLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xenoszef/DX-HUB/main/ui.lua"))() 
-local WindowOne = DarkLib:Window()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local TabNSS = WindowOne:Tab("NSS")
+OrionLib:MakeNotification({
+	Name = "DX Hub Notify",
+	Content = "DX Hub successfully loaded!",
+	Image = "rbxassetid://14758574258",	
+})
 
-TabNSS:Button("Load script", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Swireqs/DX-NSS/main/main.lua"))()
-    end)
+
+local Window = OrionLib:MakeWindow({Name = "DX Hub", HidePremium = true, SaveConfig = false, ConfigFolder = "DX Hub"})
+
+--NSS Tab--
+
+local NSSTab = Window:MakeTab({
+	Name = "NSS",
+	Icon = "rbxassetid://14758574258",
+	PremiumOnly = false
+})
+
+NSSTab:AddButton({
+	Name = "Load Script",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Swireqs/DX-NSS/main/main.lua"))()
+  	end    
+})
+
+--UI Tab--
+
+local UITab = Window:MakeTab({
+	Name = "UI",
+	Icon = "rbxassetid://14758574258",
+	PremiumOnly = false
+})
+
+UITab:AddButton({
+	Name = "Destroy UI",
+	Callback = function()
+        OrionLib:Destroy()
+  	end    
+})
+
+--Settings End--
+
+OrionLib:Init() --UI Lib End
